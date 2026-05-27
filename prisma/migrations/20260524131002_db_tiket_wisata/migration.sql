18cba25d0dabde2877d438da3dfd69f6f9b236e3
fidelaandriana <dela30130@email.com>
2026-05-27 14:38:57 +0700

    new clean migration

--- prisma/migrations/20260524131002_db_tiket_wisata/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `booking` ADD COLUMN `paymentMethod` VARCHAR(191) NULL,
-    ADD COLUMN `paymentProof` VARCHAR(191) NULL;
--- prisma/migrations/20260527073737_db_tiket_wisata/migration.sql
+++ prisma/migrations/20260527073737_db_tiket_wisata/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE `User` (
-    `id` VARCHAR(191) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
@@ -14,12 +14,12 @@ CREATE TABLE `User` (
 
 -- CreateTable
 CREATE TABLE `Wisata` (
-    `id` VARCHAR(191) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
     `location` VARCHAR(191) NOT NULL,
     `price` INTEGER NOT NULL,
-    `imageUrl` VARCHAR(191) NULL,
+    `image` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -28,20 +28,22 @@ CREATE TABLE `Wisata` (
 
 -- CreateTable
 CREATE TABLE `Booking` (
-    `id` VARCHAR(191) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `customerName` VARCHAR(191) NOT NULL,
+    `customerEmail` VARCHAR(191) NOT NULL,
+    `customerPhone` VARCHAR(191) NOT NULL,
+    `wisataId` INTEGER NOT NULL,
     `totalTicket` INTEGER NOT NULL,
     `totalPrice` INTEGER NOT NULL,
-    `bookingDate` DATETIME(3) NOT NULL,
-    `status` ENUM('PENDING', 'SUCCESS', 'CANCELED') NOT NULL DEFAULT 'PENDING',
-    `userId` VARCHAR(191) NOT NULL,
-    `wisataId` VARCHAR(191) NOT NULL,
+    `visitDate` DATETIME(3) NOT NULL,
+    `paymentMethod` VARCHAR(191) NULL,
+    `paymentProof` VARCHAR(191) NULL,
+    `status` ENUM('PENDING', 'SUCCESS', 'CANCELLED') NOT NULL DEFAULT 'PENDING',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `Booking` ADD CONSTRAINT `Booking_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Booking` ADD CONSTRAINT `Booking_wisataId_fkey` FOREIGN KEY (`wisataId`) REFERENCES `Wisata`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
