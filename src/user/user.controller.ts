f31f813222c0cd38717434e04cbad6db82c1bee8
fidelaandriana <dela30130@email.com>
2026-06-02 14:10:41 +0700

    add user swagger

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,7 +10,9 @@ import {
 import { UsersService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
 
